10001462495b7ca83151ed0dfb9986892f92edab
Nicu Taban <nictbndeveloper@gmail.com>
2023-12-17 12:17:48 +0200

    Get response as map

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -8,16 +8,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 @RequiredArgsConstructor
 @Service
 public class BeerClientImpl implements BeerClient {
-    public static final String BASE_URL = "http://localhost:8080/api/v1/beer";
+    public static final String GET_BEER_PATH = "/api/v1/beer";
+    public static final String BASE_URL = "http://localhost:8080";
     private final RestTemplateBuilder restTemplateBuilder;
     @Override
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
-        ResponseEntity<String> stringResponse = restTemplate.getForEntity(BASE_URL, String.class);
+        ResponseEntity<String> stringResponse = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, String.class);
+        ResponseEntity<Map> mapResponse = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
         System.out.println(stringResponse.getBody());
+        System.out.println(mapResponse.getBody());
         return null;
     }
 }
